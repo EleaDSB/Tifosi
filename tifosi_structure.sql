0ddc605ff73f2abdf4ec55cfb60d9d15e666a2e9
EleaDSB <bitsch.elea@gmail.com>
2026-01-14 11:27:07 +0100

    Add files via upload

---
+++ tifosi_structure.sql
@@ -0,0 +1,316 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le : mer. 14 jan. 2026 à 10:24
+-- Version du serveur : 8.0.44
+-- Version de PHP : 8.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `tifosi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `achete`
+--
+
+CREATE TABLE `achete` (
+  `id_client` int NOT NULL,
+  `id_focaccia` int NOT NULL,
+  `jour` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `boisson`
+--
+
+CREATE TABLE `boisson` (
+  `id_boisson` int NOT NULL,
+  `nom_boisson` varchar(45) NOT NULL,
+  `id_marque` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE `client` (
+  `id_client` int NOT NULL,
+  `nom_client` varchar(45) NOT NULL,
+  `age` int DEFAULT NULL,
+  `cp_client` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comprend`
+--
+
+CREATE TABLE `comprend` (
+  `id_focaccia` int NOT NULL,
+  `id_ingredient` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contient`
+--
+
+CREATE TABLE `contient` (
+  `id_menu` int NOT NULL,
+  `id_boisson` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `est_constitue`
+--
+
+CREATE TABLE `est_constitue` (
+  `id_focaccia` int NOT NULL,
+  `id_menu` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `focaccia`
+--
+
+CREATE TABLE `focaccia` (
+  `id_focaccia` int NOT NULL,
+  `nom_focaccia` varchar(45) NOT NULL,
+  `prix_focaccia` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ingredient`
+--
+
+CREATE TABLE `ingredient` (
+  `id_ingredient` int NOT NULL,
+  `nom_ingredient` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `marque`
+--
+
+CREATE TABLE `marque` (
+  `id_marque` int NOT NULL,
+  `nom_marque` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `menu`
+--
+
+CREATE TABLE `menu` (
+  `id_menu` int NOT NULL,
+  `nom_menu` varchar(45) NOT NULL,
+  `prix_menu` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `paye`
+--
+
+CREATE TABLE `paye` (
+  `id_client` int NOT NULL,
+  `id_menu` int NOT NULL,
+  `jour` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `achete`
+--
+ALTER TABLE `achete`
+  ADD PRIMARY KEY (`id_client`,`id_focaccia`,`jour`),
+  ADD KEY `id_focaccia` (`id_focaccia`);
+
+--
+-- Index pour la table `boisson`
+--
+ALTER TABLE `boisson`
+  ADD PRIMARY KEY (`id_boisson`),
+  ADD KEY `fk_boisson_marque` (`id_marque`);
+
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id_client`);
+
+--
+-- Index pour la table `comprend`
+--
+ALTER TABLE `comprend`
+  ADD PRIMARY KEY (`id_focaccia`,`id_ingredient`),
+  ADD KEY `id_ingredient` (`id_ingredient`);
+
+--
+-- Index pour la table `contient`
+--
+ALTER TABLE `contient`
+  ADD PRIMARY KEY (`id_menu`,`id_boisson`),
+  ADD KEY `id_boisson` (`id_boisson`);
+
+--
+-- Index pour la table `est_constitue`
+--
+ALTER TABLE `est_constitue`
+  ADD PRIMARY KEY (`id_focaccia`,`id_menu`),
+  ADD KEY `id_menu` (`id_menu`);
+
+--
+-- Index pour la table `focaccia`
+--
+ALTER TABLE `focaccia`
+  ADD PRIMARY KEY (`id_focaccia`);
+
+--
+-- Index pour la table `ingredient`
+--
+ALTER TABLE `ingredient`
+  ADD PRIMARY KEY (`id_ingredient`);
+
+--
+-- Index pour la table `marque`
+--
+ALTER TABLE `marque`
+  ADD PRIMARY KEY (`id_marque`);
+
+--
+-- Index pour la table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id_menu`);
+
+--
+-- Index pour la table `paye`
+--
+ALTER TABLE `paye`
+  ADD PRIMARY KEY (`id_client`,`id_menu`,`jour`),
+  ADD KEY `id_menu` (`id_menu`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `boisson`
+--
+ALTER TABLE `boisson`
+  MODIFY `id_boisson` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `client`
+--
+ALTER TABLE `client`
+  MODIFY `id_client` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `focaccia`
+--
+ALTER TABLE `focaccia`
+  MODIFY `id_focaccia` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `ingredient`
+--
+ALTER TABLE `ingredient`
+  MODIFY `id_ingredient` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `marque`
+--
+ALTER TABLE `marque`
+  MODIFY `id_marque` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id_menu` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `achete`
+--
+ALTER TABLE `achete`
+  ADD CONSTRAINT `achete_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`),
+  ADD CONSTRAINT `achete_ibfk_2` FOREIGN KEY (`id_focaccia`) REFERENCES `focaccia` (`id_focaccia`);
+
+--
+-- Contraintes pour la table `boisson`
+--
+ALTER TABLE `boisson`
+  ADD CONSTRAINT `fk_boisson_marque` FOREIGN KEY (`id_marque`) REFERENCES `marque` (`id_marque`);
+
+--
+-- Contraintes pour la table `comprend`
+--
+ALTER TABLE `comprend`
+  ADD CONSTRAINT `comprend_ibfk_1` FOREIGN KEY (`id_focaccia`) REFERENCES `focaccia` (`id_focaccia`),
+  ADD CONSTRAINT `comprend_ibfk_2` FOREIGN KEY (`id_ingredient`) REFERENCES `ingredient` (`id_ingredient`);
+
+--
+-- Contraintes pour la table `contient`
+--
+ALTER TABLE `contient`
+  ADD CONSTRAINT `contient_ibfk_1` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id_menu`),
+  ADD CONSTRAINT `contient_ibfk_2` FOREIGN KEY (`id_boisson`) REFERENCES `boisson` (`id_boisson`);
+
+--
+-- Contraintes pour la table `est_constitue`
+--
+ALTER TABLE `est_constitue`
+  ADD CONSTRAINT `est_constitue_ibfk_1` FOREIGN KEY (`id_focaccia`) REFERENCES `focaccia` (`id_focaccia`),
+  ADD CONSTRAINT `est_constitue_ibfk_2` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id_menu`);
+
+--
+-- Contraintes pour la table `paye`
+--
+ALTER TABLE `paye`
+  ADD CONSTRAINT `paye_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`),
+  ADD CONSTRAINT `paye_ibfk_2` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id_menu`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
